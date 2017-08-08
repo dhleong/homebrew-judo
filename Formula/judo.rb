a360f1ce24d2445c2aedb15d9adce1bd525be6a6
Daniel Leong <falcone88@gmail.com>
2017-08-08 08:25:20 -0400

    Update for v1.0.3

--- Formula/judo.rb
+++ Formula/judo.rb
@@ -1,8 +1,8 @@
 class Judo < Formula
   desc "A more elegant way to play in the MUD"
   homepage "https://github.com/dhleong/judo"
-  url "https://github.com/dhleong/judo/releases/download/1.0.2/judo-1.0.2.jar"
-  sha256 "a15d4759aa466feb95a04614d5b5a3ac745ab8ff8ff1005e5d66ef84f61b5b6f"
+  url "https://github.com/dhleong/judo/releases/download/1.0.3/judo-1.0.3.jar"
+  sha256 "ae88e3ad7181913300efbb9c46eb0674066aa2bc49467590ac189f683beff218"
 
   bottle :unneeded
 
