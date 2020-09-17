7d3d0cb38bd198e8801e7b408c134e57f64c2f81
Daniel Leong <me@dhleong.net>
2020-09-17 10:59:31 -0400

    Fix formatting errors from py3 release script

--- Formula/judo.rb
+++ Formula/judo.rb
@@ -1,1 +1,19 @@
-b'class Judo < Formula\n  desc "A more elegant way to play in the MUD"\n  homepage "https://github.com/dhleong/judo"\n  url "https://github.com/dhleong/judo/releases/download/1.15.2/judo-1.15.2.jar"\n  sha256 "16ac8c3169c957415b287a98808d585988d42bf88c528e7ba028ce845ab5f3c9"\n\n  bottle :unneeded\n\n  depends_on :java => "1.8+"\n\n  def install\n    libexec.install "judo-#{version}.jar"\n    bin.write_jar_script libexec/"judo-#{version}.jar", "judo"\n  end\n\n  test do\n    assert_match "judo version #{version}", pipe_output("#{bin}/judo --version")\n  end\nend\n'
+class Judo < Formula
+  desc "A more elegant way to play in the MUD"
+  homepage "https://github.com/dhleong/judo"
+  url "https://github.com/dhleong/judo/releases/download/1.15.2/judo-1.15.2.jar"
+  sha256 "16ac8c3169c957415b287a98808d585988d42bf88c528e7ba028ce845ab5f3c9"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    libexec.install "judo-#{version}.jar"
+    bin.write_jar_script libexec/"judo-#{version}.jar", "judo"
+  end
+
+  test do
+    assert_match "judo version #{version}", pipe_output("#{bin}/judo --version")
+  end
+end
